b03f17e936b7f872bc740ed44c9a51c038a9b5e6
Gorstak-ZD <113023910+Gorstak-ZD@users.noreply.github.com>
2022-12-12 00:33:35 +0100

    Add files via upload

---
+++ fix.bat
@@ -0,0 +1,9 @@
+wmic path win32_networkadapter where index=0 call disable
+wmic path win32_networkadapter where index=2 call disable
+wmic path win32_networkadapter where index=3 call disable
+wmic path win32_networkadapter where index=4 call disable
+wmic path win32_networkadapter where index=5 call disable
+wmic path win32_networkadapter where index=6 call disable
+wmic path win32_networkadapter where index=7 call disable
+wmic path win32_networkadapter where index=8 call disable
+wmic path win32_networkadapter where index=9 call disable
